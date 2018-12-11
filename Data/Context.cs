3e9a42ca5fbdd7da7e121827a427113fff9f4343
mariammorcos <45771321+mariammorcos@users.noreply.github.com>
2018-12-11 23:44:13 +0200

    Add files via upload

---
+++ Data/Context.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingProject.Data
+{
+    public class Context : DbContext
+    {
+
+        public Context(DbContextOptions<Context> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<Models.Cart> Carts { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.Cart>()
+                 .HasKey(sc => new { sc.ID, sc.Product });
+
+
+
+        }
+    }
+}
