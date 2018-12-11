5146f6acb0914326fa0362777e6f8b7cbb5d3fc1
mariammorcos <45771321+mariammorcos@users.noreply.github.com>
2018-12-11 23:45:32 +0200

    Add files via upload

---
+++ Models/Cart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingProject.Models
+{
+    public class Cart
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        public string Product { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public string Features { get; set; }
+
+        [Required]
+        public string color { get; set; }
+
+        [Required]
+        public double Total { get; set; }
+
+        public void Total()
+        {
+            
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingProject.Models
+{
+    public class User
+    {
+    }
+}
